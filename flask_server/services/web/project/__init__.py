96c4afcdd4b74f13e6e35fec869840e5c3fb9110
Tomasz Michalik <t.michalik630@gmail.com>
2022-07-13 13:46:20 +0200

    Initial commit

---
+++ flask_server/services/web/project/__init__.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+from project.models import db
+
+
+app = Flask(__name__)
+app.config.from_object("project.config.Config")
+
+db.init_app(app)
+
+
+@app.route("/")
+def hello_world():
+    return jsonify(hello="world")
