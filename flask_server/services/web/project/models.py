8ffa9e8120cde8a2206879f70086c702f4def470
Tomasz Michalik <t.michalik630@gmail.com>
2022-07-13 21:17:23 +0200

    Added games model

--- flask_server/services/web/project/models.py
+++ flask_server/services/web/project/models.py
@@ -11,3 +11,17 @@ class User(db.Model):
 
     def __init__(self, name):
         self.name = name
+
+class Game(db.Model):
+    __tablename__ = "games"
+
+    id = db.Column(db.Integer, primary_key=True)
+    created_at = db.Column(db.DateTime, nullable=False)
+    finished_at = db.Column(db.DateTime, default=False, nullable=True)
+    user1_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user2_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    user1_win_count = db.Column(db.Integer, default=0, nullable=False)
+    user2_win_count = db.Column(db.Integer, default=0, nullable=False)
+
+    def __init__(self, name):
+        self.name = id
