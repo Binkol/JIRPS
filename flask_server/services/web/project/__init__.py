a262471e3983ce29082404b6fe11c9a6380d6bf9
Tomasz Michalik <t.michalik630@gmail.com>
2022-07-15 18:03:33 +0200

    RPS through chat without database

--- flask_server/services/web/project/__init__.py
+++ flask_server/services/web/project/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, render_template, request, session
 from project.models import db, User, Game
 from flask_socketio import SocketIO, join_room, leave_room, emit
+import datetime
 
 app = Flask(__name__,
             static_folder='./static',
@@ -10,6 +11,24 @@ app.config.from_object("project.config.Config")
 db.init_app(app)
 
 socketio = SocketIO(app, cors_allowed_origins="*")
+
+games_statuses = {}
+
+def getWinner(game_status):
+    keys = list(game_status)
+    values = list(game_status.values())
+
+    if values[0] == values[1]:
+        return None
+    
+    if values[0] == "rock" and values[1] == "scissors":
+        return keys[0]
+    elif values[0] == "paper" and values[1] == "rock":
+        return keys[0]
+    elif values[0] == "scissors" and values[1] == "paper":
+        return keys[0]
+    else:
+        return keys[1]
 
 @app.route("/")
 def register():
@@ -52,6 +71,27 @@ def logout():
 @app.route("/game_room", methods=['GET', 'POST'])
 def game_room():
     session["room"] = request.form["room"]
+    user = User.query.filter_by(name=session.get('username')).first()
+    game = Game.query.filter_by(room_name=request.form["room"]).first()
+    
+    if not game:
+        new_game = Game(
+                room_name = request.form["room"],
+                created_at = datetime.datetime.now(),
+                user1_id = user.id
+                )
+        db.session.add(new_game)
+        db.session.commit()
+
+        session["player1"] = user.name
+    else:
+        game.user2_id = user.id
+        db.session.commit()
+
+        session["player2"] = user.name
+    
+    games_statuses[request.form["room"]] = {}
+
     return render_template("game_room.html", session=session)
 
 
@@ -74,3 +114,24 @@ def left(message):
     leave_room(room)
     session.clear()
     emit("status", {"msg": "username " + username + " left."})
+
+@socketio.on("selected_option", namespace="/game_room")
+def choose_rock(message):
+    room = session.get("room")
+    username = session.get("username")
+
+    games_statuses[room][username] = message["msg"]
+    emit("message", {"msg": username + " is ready."}, room=room)
+    
+    if len(games_statuses[room]) == 2:
+        winner = getWinner(games_statuses[room])
+        if winner is not None:
+            msg = "{} won with {}".format(winner, games_statuses[room][winner])
+        else:
+            msg = "Draw, {0} against {0}".format(games_statuses[room][username])
+        emit("message", {"msg": msg}, room=room)
+        games_statuses[room] = {}
+
+
+    
+
--- flask_server/services/web/project/models.py
+++ flask_server/services/web/project/models.py
@@ -1,3 +1,4 @@
+from email.policy import default
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
@@ -16,12 +17,15 @@ class Game(db.Model):
     __tablename__ = "games"
 
     id = db.Column(db.Integer, primary_key=True)
+    room_name = db.Column(db.String(128), unique=True, nullable=False)
     created_at = db.Column(db.DateTime, nullable=False)
-    finished_at = db.Column(db.DateTime, default=False, nullable=True)
-    user1_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    user2_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    finished_at = db.Column(db.DateTime, default=None, nullable=True)
+    user1_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=True, default=None)
+    user2_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=True, default=None)
     user1_win_count = db.Column(db.Integer, default=0, nullable=False)
     user2_win_count = db.Column(db.Integer, default=0, nullable=False)
 
-    def __init__(self, name):
-        self.name = id
+    def __init__(self, room_name, created_at, user1_id):
+        self.room_name = room_name
+        self.created_at = created_at
+        self.user1_id = user1_id
